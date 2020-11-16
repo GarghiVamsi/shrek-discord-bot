f4645784ae72c9f3950fdc0d7e686e4ef33994fc
GarghiVamsi <garghivamsi@gmail.com>
2020-11-16 13:16:17 -0600

    Discord bot of shrek 2 on dvd

---
+++ index.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const PREFIX = '!';
+
+bot.on('ready', () =>{
+    console.log('Shrek is ready to be green!');
+})
+
+bot.on('message', message =>{
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    if (!message.content.startsWith(PREFIX)) return;
+    if (message.author.bot) return;
+    
+
+
+    switch(args[0]){
+        case 'shrek':
+            number = 1081;
+            imageNumber = Math.floor(Math.random() * (number - 1 + 1)) +1;
+            message.channel.send({files: ["./images/" + imageNumber + ".png"]})
+        break;
+    }
+
+})
+
+bot.login('Nzc3OTQzNTQ3NTAzNDQ0MDUw.X7KyYw.NARHFnldcu5o8PRyLE-b_NVMDnk');
